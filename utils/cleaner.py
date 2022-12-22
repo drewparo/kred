1c51b3ada604a4e6385874de7db68731a83c3f66
drewparo <parolinandrea2@gmail.com>
2022-12-22 12:58:52 +0100

    Fixes to Exception in clearner.py

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -60,8 +60,7 @@ def cleaner(config):
             dst_train = '/'.join(config["data"]["train_news"].split('/')[:-1]) + '/'
             shutil.copy(src_train, dst_train)
         return config
-    except Exception as e:
-        print(e)
+    except:
         print('Replacing name in config')
         config['data']['train_news'] = 'tmp/train_' + config["data"]["train_news"].split('/')[-1]
         config['data']['valid_news'] = 'tmp/test_' + config["data"]["valid_news"].split('/')[-1]
