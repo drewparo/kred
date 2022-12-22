21ba5dc60a0c8d759074a2a1cefcb61dfa3418c4
drewparo <parolinandrea2@gmail.com>
2022-12-22 12:54:49 +0100

    Fixes to OSError

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -60,7 +60,7 @@ def cleaner(config):
             dst_train = '/'.join(config["data"]["train_news"].split('/')[:-1]) + '/'
             shutil.copy(src_train, dst_train)
         return config
-    except OSError:
+    except OSError as e:
         print('Could not overwrite old file, replacing name in config')
         config['data']['train_news'] = 'tmp/train_' + config["data"]["train_news"].split('/')[-1]
         config['data']['valid_news'] = 'tmp/test_' + config["data"]["valid_news"].split('/')[-1]
