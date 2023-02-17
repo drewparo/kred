9776ac93fc932ce7213be23d46781525d6731957
drewparo <parolinandrea2@gmail.com>
2023-02-17 17:13:03 +0100

    Adding method for summarization

--- utils/extract_wikidata.py
+++ utils/extract_wikidata.py
@@ -4,11 +4,22 @@ import spacy
 import ast
 import json
 from nltk.tokenize import sent_tokenize, word_tokenize
-nlp = spacy.load("en_core_web_sm")
+from string import punctuation
+from txtai.pipeline import Summary
+from nltk.corpus import stopwords
+import spacy
+from heapq import nlargest
+nlp = spacy.load('en_core_web_sm')
+
+nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+STOP_WORDS = set(stopwords.words('english'))
+stopwords = list(STOP_WORDS)
+summary = Summary()
+
 
 def get_ner(text):
     sent = nltk.word_tokenize(text)
@@ -29,25 +40,46 @@ def get_wikidata_id(entity):
 
 def extract_significant_sentences(text):
     try:
-        # Tokenize the text into sentences
-        sentences = sent_tokenize(text)
+        global summary, punctuation
+        if text is None or text == '':
+            return ''
+        outcome = text
+        text_length = len(text)
+        doc = nlp(text)
+        tokens = [token.text for token in doc]
+        punctuation = punctuation + '\n'
+        word_frequencies = {}
 
-        # Create a dictionary to store the importance of each sentence
-        sentence_importance = {}
+        for word in doc:
+            if word.text.lower() not in stopwords:
+                if word.text.lower() not in punctuation:
+                    if word.text not in word_frequencies.keys():
+                        word_frequencies[word.text] = 1
+                    else:
+                        word_frequencies[word.text] += 1
+        max_frequency = max(word_frequencies.values())
 
-        # Loop through each sentence and calculate its importance based on word frequency
-        for sentence in sentences:
-            words = nltk.word_tokenize(sentence)
-            frequency = nltk.FreqDist(words)
-            sentence_importance[sentence] = frequency.most_common(1)[0][1]
+        for word in word_frequencies.keys():
+            word_frequencies[word] = word_frequencies[word] / max_frequency
+        sentence_tokens = [sent for sent in doc.sents]
+        sentence_scores = {}
+        for sent in sentence_tokens:
+            for word in sent:
+                if word.text.lower() in word_frequencies.keys():
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
 
-        # Sort the sentences by their importance
-        sorted_sentences = sorted(sentence_importance.items(), key=lambda x: x[1], reverse=True)
+        select_length = int(len(sentence_tokens) * 0.5)
 
-        # Return the most important sentence
-        return sorted_sentences[0][0]
-    except TypeError:
-        return ""
+        summary1 = nlargest(select_length, sentence_scores,
+                            key=sentence_scores.get)
+        sumup = summary(summary1[0].text)
+        return sumup
+    except Exception as e:
+        print('error')
+        return text
 
 
 def get_extract(text, wikidata_ids):
